4e88ef6c0cbb40b50eeedc2af855c22ff6d369f5
sadman249 <mdsadman249@gmail.com>
2024-12-31 08:00:47 +0600

    added the firebase & proces.env.url

--- src/pages/Home/Testimonials/Testimonials.jsx
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -16,7 +16,7 @@ const Testimonials = () => {
     const [reviews, setReviews] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:5000/reviews')
+        fetch(`${import.meta.env.VITE_API_URL}/reviews`)
             .then(res => res.json())
             .then(data => setReviews(data))
     }, [])
